69971840910e9ad80d6176b11c5c9aa765fab95b
daniel nahmani <101664115+danieln1998@users.noreply.github.com>
2023-05-09 20:39:08 +0300

    Add files via upload

---
+++ database.js
@@ -0,0 +1,30 @@
+
+
+const mysql = require('mysql2');
+const fs= require('fs');
+var dotenv=require('dotenv');
+dotenv.config();
+
+const connection= mysql.createConnection({host:process.env.HOST,
+ user:process.env.USER,
+  password:process.env.PASSWORD,
+   database:process.env.DATABASE,
+    port:process.env.PORT,
+	charset: 'utf8mb4',
+	//  ssl:{ca:fs.readFileSync("DigiCertGlobalRootCA.crt.pem")}
+	
+	
+	});
+
+connection.connect(function(error){
+	if(error)
+	{
+		throw error;
+	}
+	else
+	{
+		console.log('MySQL Database is connected Successfully');
+	}
+});
+
+module.exports = connection;
